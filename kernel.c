e9efa8f592f5a552668198a958209958f61da6ec
Krillin <kasmiabdoullah@gmail.com>
2025-03-03 13:00:38 +0100

    .

--- kernel.c
+++ kernel.c
@@ -32,8 +32,8 @@ void	strncpy(char *dest, char *src, size_t size)
 
 void kernel_main() {
     volatile unsigned char *video_memory = (unsigned char *)0xB8000; // Video memory address
-    video_memory[0] = '4'; // Write '4' to the screen
-    video_memory[1] = 0x07; // Light gray on black background
-    video_memory[2] = '2'; // Write '2' to the screen
-    video_memory[3] = 0x07; // Light gray on black background
+    video_memory[0] = '4';
+    video_memory[1] = 0x07;
+    video_memory[2] = '2';
+    video_memory[3] = 0x07;
 }
